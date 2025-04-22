dfe7f19eedce101b7ad1e0e50be5c22601ee9f18
layon <locosilia5@gmail.com>
2025-04-22 20:54:13 -0300

    Começando a construir o código em Javascript para atribuir funcionalidades e botões em cada categoria

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('gastos-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const descricao = document.getElementById('descricao').value;
+    const valor = parseFloat(document.getElementById('valor').value);
+    const categoria = document.getElementById('categoria').value;
+    
+    
+});
+
